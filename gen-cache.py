7339c16106f48191b159de1906229799facd56ec
Mike Purvis <mpurvis@clearpath.ai>
2020-08-28 21:37:37 +0000

    Fix dependency serialization in gen-cache.

--- gen-cache.py
+++ gen-cache.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from colcon_core.dependency_descriptor import DependencyDescriptor
 from colcon_core.package_discovery import discover_packages
 from colcon_core.package_selection import add_arguments as add_packages_arguments
 from colcon_core.package_identification import get_package_identification_extensions
@@ -15,11 +16,18 @@ args = parser.parse_args()
 extensions = get_package_identification_extensions()
 descriptors = discover_packages(args, extensions)
 
+def dependency_str(dep):
+    if isinstance(dep, DependencyDescriptor):
+        return dep.name
+    elif isinstance(dep, str):
+        return dep
+    raise ValueError("Unexpected dependency type.")
+
 def descriptor_output(d):
     depends_output = {}
     for deptype in ('build', 'run', 'test'):
         if deptype in d.dependencies and d.dependencies[deptype]:
-            depends_output[deptype] = sorted([dep.name for dep in d.dependencies[deptype]])
+            depends_output[deptype] = sorted([dependency_str(dep) for dep in d.dependencies[deptype]])
     return {
         'type': d.type,
         'depends': depends_output
