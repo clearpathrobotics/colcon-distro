408b23ac7eb3075b109372699ed63185fd8de09e
Mike Purvis <mpurvis@clearpath.ai>
2020-09-10 01:06:26 +0000

    Reasonable sanic defaults, including disabling uvloop.

--- colcon_distro/server.py
+++ colcon_distro/server.py
@@ -11,7 +11,23 @@ from .model import Model
 from .vendor.compress import Compress
 
 
+# For now, revert to the non-uv event loop, as uvloop gives weird error
+# messages around how the process management in the download module is
+# implemented.
+asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
 app = sanic.Sanic(__name__)
+
+# We deal in single requests; there's not advantage in them staying open.
+app.config.KEEP_ALIVE = False
+
+# Requests are tiny and should arrive quickly.
+app.config.REQUEST_TIMEOUT = 5
+
+# In pathological cases where nothing is cached, it can take a long time
+# to generate a response.
+app.config.RESPONSE_TIMEOUT = 300
+
+# Compress responses with gzip or brotli as acceptable to the client.
 Compress(app)
 
 @app.route("/get/<dist>/<ref:path>.json")
