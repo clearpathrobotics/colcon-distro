22a1a67e911c5da186a13290b421f2a8e776efbb
Mike Purvis <mpurvis@clearpath.ai>
2020-09-11 19:45:04 +0000

    Shared, mutexed database connection.

--- colcon_distro/database.py
+++ colcon_distro/database.py
@@ -1,7 +1,18 @@
 
 import aiosqlite
+import asyncio
+import contextlib
+import logging
 import pkg_resources
 import sqlite3
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# TODO: Would be interesting to profile what the overhead is of passing all these small
+# sqlite operations to the other thread to do them "asyncronously". We could probably
+# save a bunch of this is we did the insertions in batches.
 
 
 class Database:
@@ -26,27 +37,34 @@ class Database:
     INSERT INTO set_repo_states (set_id, repo_state_id) VALUES (?, ?)"""
 
     def __init__(self, config):
-        self.filepath = config.get_database_filepath()
-        if not self.filepath.exists():
-            self.initialize()
+        filepath = config.get_database_filepath()
+        if not filepath.exists():
+            self.initialize(filepath)
+        self.connection = Connection(filepath, self.connect_fn)
 
-    def initialize(self):
+    def initialize(self, filepath):
         """
         Initializes a new empty database; this only ever happens at startup, so we
         just do it synchronously.
         """
         queries = pkg_resources.resource_string(__name__, self.SCHEMA_SCRIPT).decode()
-        db = sqlite3.connect(self.filepath)
+        db = sqlite3.connect(filepath)
         db.executescript(queries)
         db.commit()
         db.close()
+
+    async def connect_fn(self, db):
+        """
+        Callback function for anything we'd like to execute on a newly-opened database connection.
+        """
+        await db.execute(self.PRAGMA_FOREIGN_KEYS)
 
     async def fetch_set(self, dist_name, name):
         """
         Return either a full set of repo_state rows if the set is in the
         database, or the empty set if it is not.
         """
-        async with aiosqlite.connect(self.filepath) as db:
+        async with self.connection() as db:
             await db.execute(self.FETCH_SET_QUERY, (dist_name, name))
             cursor = await db.execute(self.FETCH_SET_QUERY, (dist_name, name))
             return await cursor.fetchall()
@@ -56,7 +74,7 @@ class Database:
         Get a single repo state, returning a tuple that is the row id and json string,
         or None if the row is not found.
         """
-        async with aiosqlite.connect(self.filepath) as db:
+        async with self.connection() as db:
             cursor = await db.execute(self.FETCH_REPO_STATE_QUERY, (name, typename, url, version))
             result = await cursor.fetchall()
             return result[0] if result else None
@@ -66,8 +84,7 @@ class Database:
         Insert a repo state, returning the row id for it. If the row already exists,
         this query will fail due to db constraints.
         """
-        async with aiosqlite.connect(self.filepath) as db:
-            await db.execute(self.PRAGMA_FOREIGN_KEYS)
+        async with self.connection() as db:
             cursor = await db.execute(self.INSERT_REPO_STATE_QUERY, (name, typename, url, version, json_str))
             row_id = cursor.lastrowid
             await db.commit()
@@ -78,9 +95,53 @@ class Database:
         Insert a new set row from dist_name, name, and set of ids, all of which must
         exist in the repo states table or this query will fail due to db constraints.
         """
-        async with aiosqlite.connect(self.filepath) as db:
-            await db.execute(self.PRAGMA_FOREIGN_KEYS)
+        async with self.connection() as db:
             cursor = await db.execute(self.INSERT_SET_QUERY, (dist_name, name, None))
             set_id = cursor.lastrowid
             await db.executemany(self.INSERT_SET_REPO_STATES_QUERY, [(set_id, r) for r in repo_state_ids])
             await db.commit()
+
+
+class Connection:
+    """
+    This manager class provides a few important capabilities to our sqlite connection.
+    First, it mutexes it, so that commit() from one coroutine don't get interleaved with
+    queries from another, since sqlite has no built in concept of there being multiple
+    clients or concurrent transactions going on. Second, it provides a timeout which
+    ensures we always close the database file after a brief period of inactivity, since
+    the server process doesn't end cleanly if the database connection is left open, and
+    this is nicer than closing and reopening it between every operation.
+    """
+    CONNECTION_TIMEOUT=1.0
+
+    def __init__(self, filepath, connect_fn=None):
+        # Lazy-initialize all async stuff so we don't get the wrong loop if this
+        # object is constructed ahead of the loop starting.
+        self._handle = None
+        self._lock = None
+        self._timeout_task = None
+        self._filepath = filepath
+        self._connect_fn = connect_fn
+
+    @contextlib.asynccontextmanager
+    async def __call__(self):
+        self._lock = self._lock or asyncio.Lock()
+        async with self._lock:
+            if self._timeout_task:
+                self._timeout_task.cancel()
+                self._timeout_task = None
+            if not self._handle:
+                logger.info(f"Opening database at {self._filepath}.")
+                self._handle = await aiosqlite.connect(self._filepath)
+                if self._connect_fn:
+                    await self._connect_fn(self._handle)
+            yield self._handle
+            self._timeout_task = asyncio.create_task(self._connection_timeout())
+
+    async def _connection_timeout(self):
+        await asyncio.sleep(self.CONNECTION_TIMEOUT)
+        async with self._lock:
+            logger.info("Closing database.")
+            await self._handle.close()
+            self._handle = None
+            self._timeout_task = None
