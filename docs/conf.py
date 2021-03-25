d7c6ee7c1e28366066b42beca7aa5969beb3416c
Mike Purvis <mpurvis@clearpath.ai>
2021-03-25 04:19:50 +0000

    Sphinx docs bootstrap.

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../'))
+
+project = 'colcon-distro'
+copyright = '2021 Clearpath'
+author = 'Mike Purvis'
+
+# The full version, including alpha/beta/rc tags
+release = ''
+
+extensions = [
+]
+
+html_theme = 'alabaster'
+
+#exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+#html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+colcon-distro
+=============
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
